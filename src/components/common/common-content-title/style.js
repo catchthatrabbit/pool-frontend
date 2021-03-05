6d146a1a773953868aea016c383c99e6a0e73d53
mano92fuentesjimene <m92fuentes@gmail.com>
2021-03-05 14:45:24 -0500

    fix style footer

--- src/components/common/common-content-title/style.js
+++ src/components/common/common-content-title/style.js
@@ -5,10 +5,11 @@ const WrapperStyled = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 1.5rem 0;
-  @media screen and ${({ theme }) => theme.mediaQueriesMinWidth.mobileL} {
-    flex-direction: row;
-  }
+  // margin: 1.5rem 0;
+  flex-flow: row;
+  // @media screen and (min-width: 1320px) {
+  //   flex-direction: row;
+  // }
 `;
 const TitleStyled = styled.h2`
   font-size: min(max(1.25vw, 18px), 24px);
@@ -16,7 +17,7 @@ const TitleStyled = styled.h2`
   white-space: nowrap;
 `;
 const ImageStyled = styled.img`
-  @media screen and ${({ theme }) => theme.mediaQueriesMinWidth.mobileL} {
+  @media screen and (min-width: 1320px) {
     margin-right: 1rem;
   }
 `;
