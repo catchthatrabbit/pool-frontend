0455babee3d8580bd94a48871b76b0280dc04636
mano92fuentesjimene <m92fuentes@gmail.com>
2021-03-05 11:50:06 -0500

    add props to the style in Text component

--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -3,29 +3,6 @@ import { colorVariables, fonts } from 'styles/variables';
 
 import styled from 'styled-components';
 
-const StyledText = styled.text<IProps>`
-  font-size: ${(props) =>
-    props.fontSize === 'tiny' && '10px' ||
-    props.fontSize === 'small' && '12px' ||
-    props.fontSize === 'medium' && '14px' ||
-    props.fontSize === 'very-large' && '24px' ||
-    props.fontSize === 'ultra-large' && '48px' ||
-    '18px'
-};
-  font-family: ${(props) => (props.fontFamily === 'primary' ? fonts.primary : fonts.secondary)};
-  color: ${(props) =>
-    props.color === 'white' && colorVariables.white ||
-    props.color === 'apple' && colorVariables.apple ||
-    colorVariables.santasGray
-};
-  font-style: ${(props) => (props.italic ? 'italic' : 'normal')};
-  font-weight: ${(props) =>
-    props.fontWeight === 'bold' && 'bold' ||
-    props.fontWeight === 'light' && '100' ||
-    'normal'
-};
-`;
-
 interface IProps {
   size: 'tiny' | 'small' | 'medium' | 'large' | 'very-large' | 'ultra-large',
   fontFamily: 'primary' | 'secondary',
@@ -33,6 +10,29 @@ interface IProps {
   italic?: boolean,
   fontWeight?: 'bold' | 'normal' | 'light'
 }
+
+const StyledText = styled.text < IProps > `
+  font-size: ${(props: IProps) =>
+    props.size === 'tiny' && '10px' ||
+    props.size === 'small' && '12px' ||
+    props.size === 'medium' && '14px' ||
+    props.size === 'very-large' && '24px' ||
+    props.size === 'ultra-large' && '48px' ||
+    '18px'
+};
+  font-family: ${(props: IProps) => (props.fontFamily === 'primary' ? fonts.primary : fonts.secondary)};
+  color: ${(props: IProps) =>
+    props.color === 'white' && colorVariables.white ||
+    props.color === 'apple' && colorVariables.apple ||
+    colorVariables.santasGray
+};
+  font-style: ${(props: IProps) => (props.italic ? 'italic' : 'normal')};
+  font-weight: ${(props: IProps) =>
+    props.fontWeight === 'bold' && 'bold' ||
+    props.fontWeight === 'light' && '100' ||
+    'normal'
+};
+`;
 
 const Text: FC<IProps> = ({
   children,
@@ -43,7 +43,7 @@ const Text: FC<IProps> = ({
   fontWeight = 'normal',
 }) => (
   <StyledText
-    fontSize={size}
+    size={size}
     fontFamily={fontFamily}
     color={color}
     italic={italic}
