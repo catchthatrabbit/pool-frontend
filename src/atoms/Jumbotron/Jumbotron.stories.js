a32f98adf400101662a91252ed22b44b060dcabb
mano92fuentesjimene <m92fuentes@gmail.com>
2021-03-10 11:10:50 -0500

    fix style in Jumbotron component

--- src/atoms/Jumbotron/Jumbotron.stories.js
+++ src/atoms/Jumbotron/Jumbotron.stories.js
@@ -1,11 +1,19 @@
-// import React from 'react';
-// import Jumbotron from './Jumbotron';
-//
-// export default {
-//   title: 'Jumbotron',
-//   component: Jumbotron,
-// };
-//
-// export const JumbotronGeneral = () => (
-//   <Jumbotron />
-// );
+import React from 'react';
+import styled from 'styled-components';
+import Jumbotron from './Jumbotron';
+
+export default {
+  title: 'Jumbotron',
+  component: Jumbotron,
+};
+
+const Container = styled.div`
+  width: 1920px;
+  height: 934px;
+`;
+
+export const JumbotronGeneral = () => (
+  <Container>
+    <Jumbotron />
+  </Container>
+);
--- src/atoms/Jumbotron/Jumbotron.tsx
+++ src/atoms/Jumbotron/Jumbotron.tsx
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 import { colorVariables, fonts } from 'styles/variables';
 
 import Button from 'atoms/Button/Button';
+import Text from 'atoms/Text/Text';
 import MapButton from 'atoms/MapButton/MapButton';
 import { Arrow } from 'atoms/icons';
 import InfoBox from 'atoms/InfoBox/InfoBox';
@@ -25,7 +26,7 @@ const ScrollStyle = styled.div`
   height: 8px;
   background: ${colorVariables.white};
   border-radius: 25%;
-  animation: scroll 2.2s cubic-bezier(0.15, 0.41, 0.69, 0.94) infinite;
+  animation: ${scrollTranslate} 2.2s cubic-bezier(0.15, 0.41, 0.69, 0.94) infinite;
 `;
 const MouseStyle = styled.div`
    width: 3px;
@@ -34,7 +35,6 @@ const MouseStyle = styled.div`
    padding: 8px 10px;
    border: 2px solid ${colorVariables.gunPowder};
    border-radius: 20px;
-   ${ScrollStyle}
 `;
 const MouseContainerStyle = styled.div`
   position: absolute;
@@ -45,8 +45,7 @@ const MouseContainerStyle = styled.div`
   width: 24px;
   height: 64px;
   left: calc(50% - 24px);
-  bottom: 0;
-  ${MouseStyle};
+  bottom: 5%;
   @media screen and (min-width: 1200px) {
     display: flex;
   }
@@ -55,147 +54,104 @@ const MouseContainerStyle = styled.div`
 const LocationStyle = styled.div`
    position: absolute;
    top: 50%;
-   transform: translateY(-50%);
+   // transform: translateY(-50%);
    font-weight: 600;
    font-size: 10px;
    padding: 15px 10px;
    min-width: min(12vw, 215px);
    @media screen and (min-width: 0) and(max-width:340px) {
-      transform: translateX(-50%);
+      // transform: translateX(-50%);
    }
    @media screen and (min-width: 840px) {
-      transform: translateX(-50%);
+      // transform: translateX(-50%);
       font-size: 12px;
       padding: 20px;
    }
    @media screen and (min-width: 1200px) {
-      transform: translateX(-50%);
+      // transform: translateX(-50%);
       font-size: 12px;
       padding: 20px;
    }
 `;
 const USStyle = styled(LocationStyle)`
   left: 0;
-    @media screen and (min-width: 0) and(max-width:340px) {
-      left: 20%;
-      top: 10%;
-   }
-   @media screen and (min-width: 840px) {
-      top: 25%;
-      left: 15%;
-   }
-   @media screen and (min-width: 1200px) {
-      top: 33%;
-      left: 39%;
-   }
+  top: 25%;
+  left: 35%;
+   //  @media screen and (min-width: 0) and(max-width:340px) {
+   //    left: 20%;
+   //    top: 10%;
+   // }
+   // @media screen and (min-width: 840px) {
+   //    top: 25%;
+   //    left: 15%;
+   // }
+   // @media screen and (min-width: 1200px) {
+   //    top: 33%;
+   //    left: 39%;
+   // }
 `;
 const EUStyle = styled(LocationStyle)`
-  left: 50%;
-  transform: translate(-50%, -50%);
-  @media screen and (min-width: 0) and(max-width:340px) {
-      top: 50%;
-      transform: translate(-50%, -50%);
-   }
-   @media screen and (min-width: 840px) {
-      top: 25%;
-      left: 50%;
-   }
-   @media screen and (min-width: 1200px) {
-      top: 33%;
-      left: 61%;
-   }
+  top: 25%;
+  left: 58%;
+  // transform: translate(-50%, -50%);
+  // @media screen and (min-width: 0) and(max-width:340px) {
+  //     top: 50%;
+  //     transform: translate(-50%, -50%);
+  //  }
+  //  @media screen and (min-width: 840px) {
+  //     top: 25%;
+  //     left: 50%;
+  //  }
+  //  @media screen and (min-width: 1200px) {
+  //     top: 33%;
+  //     left: 61%;
+  //  }
 `;
 const APStyle = styled(LocationStyle)`
-  right: 0;
-  @media screen and (min-width: 0) and(max-width:340px) {
-      left: 80%;
-      top: 90%;
-      transform: translate(-50%, -100%);
-   }
-   @media screen and (min-width: 840px) {
-      top: 35%;
-      left: 78%;
-   }
-   @media screen and (min-width: 1200px) {
-       top: 42%;
-       left: 82%;
-   }
+  top: 35%;
+  left: 77%;
+  // @media screen and (min-width: 0) and(max-width:340px) {
+  //     left: 80%;
+  //     top: 90%;
+  //     // transform: translate(-50%, -100%);
+  //  }
+  //  @media screen and (min-width: 840px) {
+  //     top: 35%;
+  //     left: 78%;
+  //  }
+  //  @media screen and (min-width: 1200px) {
+  //      top: 42%;
+  //      left: 82%;
+  //  }
 `;
 const MapStyle = styled.div`
-   position: relative;
+   position: absolute;
+   top: 50px;
+   height: 934px; 
+   // position: relative;
    width: 100%;
-   background-image: url("/images/map_bg.png");
-   ${USStyle};
-   ${EUStyle};
-   ${APStyle};
-   ${MouseContainerStyle};
-   ${scrollTranslate};
+   background: url('/images/map_bg.png')
 `;
 const JumbotronStyle = styled.div`
+  height: 934px; 
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
-  ${MapStyle};
-  
-  h1 {
-    margin: 20px 0 0;
-    font-size: min(max(2.2vw, 27px), 42px);
-    text-align: center;
-    order: -1;
-    span {
-      white-space: nowrap;
-      color: ${colorVariables.apple};
-    }
-    @media screen and (min-width: 900px) {
-      order: 0;
-    }
-  }
-  p {
-    margin: 30px 0;
-    line-height: min(max(1.5vw, 20px), 29px);
-    font-family: ${fonts.secondary};
-    font-size: min(max(1.2vw, 15px), 24px);
-    color: ${colorVariables.santasGray};
-    text-align: center;
-  }
-  @media screen and (min-width: 1200px) {
-    h1,
-    p {
-      position: absolute;
-      align-self: start;
-      text-align: start;
-      width: 20%;
-    }
-    h1 {
-      bottom: 60%;
-    }
-    p {
-      bottom: 15%;
-    }
-    button {
-      position: absolute;
-      bottom: 5%;
-      left: 10%;
-      transform: translateX(-50%);
-    }
-  }
   ul {
+    z-index: 1;
     display: flex;
     flex-direction: column;
     margin: 60px 0 0;
     padding: 0;
     list-style-type: none;
-    li {
-      margin-bottom: 20px;
-    }
     @media screen and (min-width: 450px) {
       flex-flow: row wrap;
       justify-content: space-evenly;
       width: 100%;
       li:not(:last-child) {
-        margin-right: 5px;
+        margin-right: 65px;
         margin-bottom: 50px;
       }
     }
@@ -207,6 +163,49 @@ const JumbotronStyle = styled.div`
         margin: 0;
       }
     }
+  }
+`;
+const StyledTitleTex = styled.div`
+  bottom: 60%;
+  margin: 20px 0 0;
+  order: -1;
+  word-break: break-all;
+  span {
+   white-space: nowrap;
+  }
+`;
+
+const StyledButtonContent = styled.div`
+  width: 100%;
+  margin-top: 87px;
+  button {
+    margin-left: 20px;
+  }
+`;
+
+const StyledInfoComponent = styled.div`
+  margin-left: 140px;
+  top: 20%;
+  position: absolute;
+  align-self: start;
+  text-align: start;
+  width: 16%;
+  h1 {
+    bottom: 60%;
+    margin: 20px 0 0;
+    order: -1;
+    span {
+      white-space: nowrap;
+      color: ${colorVariables.apple};
+    }
+  }
+  p {
+    bottom: 25%;
+    margin: 30px 0;
+    line-height: 29px;
+    font-family: ${fonts.secondary};
+    font-size: 24px;
+    color: ${colorVariables.santasGray};
   }
 `;
 
@@ -282,6 +281,13 @@ const Jumbotron: FC = () => {
   }
   return (
     <JumbotronStyle>
+      <ul>
+        {boxesInfo.map(({ title, value }) => (
+          <li key={title}>
+            <InfoBox title={title} value={value} />
+          </li>
+        ))}
+      </ul>
       <MapStyle>
         {renderButtons()}
         <MouseContainerStyle>
@@ -291,33 +297,32 @@ const Jumbotron: FC = () => {
           <Arrow />
         </MouseContainerStyle>
       </MapStyle>
-      <h1>
-        Dedicated Pool for <span>CORE COIN</span>
-      </h1>
-      <p>
-        We have several locations for you to select from.
-        <br />
-        Please select one of the locations to start your mines today!
-        <br />
-        <br />
-        PPLNS System
-        <br />
-        2% Pool Fee
-        <br />
-        Payout Threshhold 20 XCB
-        <br />
-        Payout 3 times per day
-        <br />
-      </p>
-      <Button>Start Mining</Button>
+      <StyledInfoComponent>
+        <StyledTitleTex>
+          <Text size="ultra-large" italic>Dedicated</Text>
+          <Text size="ultra-large" italic>Pool for</Text>
+          <Text size="ultra-large" color="apple" italic>CORE COIN</Text>
+        </StyledTitleTex>
+        <p>
+          We have several locations for you to select from.
+          <br />
+          Please select one of the locations to start your mines today!
+          <br />
+          <br />
+          PPLNS System
+          <br />
+          2% Pool Fee
+          <br />
+          Payout Threshhold 20 XCB
+          <br />
+          Payout 3 times per day
+          <br />
+        </p>
+        <StyledButtonContent>
+          <Button>Start Mining</Button>
+        </StyledButtonContent>
+      </StyledInfoComponent>
       {/* TODO change to a reusable component */}
-      <ul>
-        {boxesInfo.map(({ title, value }) => (
-          <li key={title}>
-            <InfoBox title={title} value={value} />
-          </li>
-        ))}
-      </ul>
     </JumbotronStyle>
   );
 };
