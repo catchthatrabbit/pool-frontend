741aeaf2783f577fc516b5d0c5fc1f110de04833
mano92fuentesjimene <m92fuentes@gmail.com>
2021-03-15 16:25:57 -0400

    fix style in home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,13 @@ import { useRouter } from 'next/router';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer';
 
+import styled from 'styled-components';
 import RootProvider from '../providers';
 
+const StyledMain = styled.main`
+  margin-right: 0;
+  margin-left: 0;
+`;
 function MyApp({ Component, pageProps }) {
   const { pathname } = useRouter();
   const isErrorPage = pathname === '/404' || pathname === '/_error';
@@ -11,9 +16,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <RootProvider>
       {!isErrorPage && <Header />}
-      <main>
+      <StyledMain>
         <Component {...pageProps} />
-      </main>
+      </StyledMain>
       {!isErrorPage && <Footer />}
     </RootProvider>
   );
