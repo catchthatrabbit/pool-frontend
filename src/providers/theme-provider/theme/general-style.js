08e7def602834455f16ba27de8749038146a47e3
mano92fuentesjimenez <m92fuentes@gmail.com>
2021-03-16 09:10:14 -0400

    style scrollbar

--- src/providers/theme-provider/theme/general-style.js
+++ src/providers/theme-provider/theme/general-style.js
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
+import { colorVariables } from '../../../styles/variables'
+import applyTransparence from '../../../helpers/transparentize'
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -32,6 +34,32 @@ const GlobalStyle = createGlobalStyle`
       width: 90%;
       max-width: 1640px;
     }
+  }
+  
+  ::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+  ::-webkit-scrollbar-button {
+    width: 13px;
+    height: 13px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${colorVariables.gunPowder};
+    border: 0 none ${colorVariables.white};
+    border-radius: 47px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${applyTransparence(0.7, colorVariables.gunPowder)};
+  }
+  ::-webkit-scrollbar-track {
+    background: ${colorVariables.santasGray};
+    border: 0 none;
+    border-radius: 50px;
+    background: ${colorVariables.charade};
+  }
+  ::-webkit-scrollbar-corner {
+    background: transparent;
   }
 `
 
