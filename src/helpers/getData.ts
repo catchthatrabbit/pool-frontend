7e9bbdd9037d0042ba01d43d27d91027bea3602e
mano92fuentesjimenez <m92fuentes@gmail.com>
2021-04-01 20:54:25 -0400

    delete unused code

--- src/helpers/getData.ts
+++ src/helpers/getData.ts
@@ -1,16 +1,4 @@
 import { StatsData, JumbotronData, TableData } from 'mockData/homePageData'
-import path from 'path'
-import { promises as fs } from 'fs'
-
-async function getFile() {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const filenames = await fs.readdir(postsDirectory)
-
-  const posts = filenames.map(async (filename) => {
-    const filePath = path.join(postsDirectory, filename)
-    return await fs.readFile(filePath, 'utf8')
-  })
-}
 
 export default async function getData(url) {
   let data
