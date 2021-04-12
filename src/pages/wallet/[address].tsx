f4d2051bbc176739d08581ddeb7516518d3563d7
mano92fuentesjimenez <m92fuentes@gmail.com>
2021-04-12 14:09:18 -0400

    add page not found

--- src/pages/wallet/[address].tsx
+++ src/pages/wallet/[address].tsx
@@ -30,6 +30,7 @@ import useGoToWallet from 'hooks/useGoToWallet'
 import { useRouter } from 'next/router'
 import Loading from '@components/Loading/Loading'
 import { minWidth } from 'helpers/responsive'
+import NotFound from '@components/NotFound/NotFound'
 
 const ContainerStyled = styled.div`
   z-index: 1;
@@ -204,6 +205,7 @@ export const getStaticPaths: GetStaticPaths = defaultGetStaticPaths
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   // const props = fetch(`/api/${address}`)
+  // const errorCode = props.ok ? false : props.statusCode
 
   console.log(params)
 
@@ -241,6 +243,9 @@ const Wallet: FC<any> = (props) => {
   if (router.isFallback) {
     return <Loading />
   }
+  // if (errorCode) {
+  //   return <NotFound />
+  // }
   return (
     <>
       <Background />
