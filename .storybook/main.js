70ad922a0e449b34149d4f2910825183e3dab525
Rastislav <rastislav@wallit.eu>
2022-01-23 23:50:54 +0100

    Revert changes
    Revert environment changes to further investigation

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ module.exports = {
     '@storybook/addon-controls',
   ],
   typescript: {
-	reactDocgen: 'none',
 	check: true, // type-check stories during Storybook build
   },
   webpackFinal: (...args) =>
