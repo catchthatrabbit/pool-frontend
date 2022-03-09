8e5f7a6bfa4586f2ac4c187d7a5f14cbdf899ca7
araad-wallit <araad.rashedi@wallit.eu>
2022-03-09 19:53:11 +0330

    fix: simplifing text function helper

--- src/helpers/text.ts
+++ src/helpers/text.ts
@@ -5,7 +5,15 @@ import ago from './ago'
 import currency from './currency'
 import numberFormat from 'helpers/numberFormat'
 
-type TypeNumber = 'hashSpeed' | 'hash' | 'percent' | 'number' | 'euro' | 'xcb' | 'time' | 'ago'
+type TypeNumber =
+  | 'hashSpeed'
+  | 'hash'
+  | 'percent'
+  | 'number'
+  | 'euro'
+  | 'xcb'
+  | 'time'
+  | 'ago'
 
 export type InfoBoxItem = {
   title: string
@@ -13,62 +21,71 @@ export type InfoBoxItem = {
   type: TypeNumber
 }
 export type TextType = {
-  value: number
+  value: string
   prefix: string
   suffix: string
 }
 
-export default function getText(type, value): TextType {
-  switch (type) {
-    case 'hashSpeed':
-    case 'hash':
-      return {
-        value: numberToString(siFormat(value,2)),
-        prefix: '',
-        suffix: 'h/s',
-      }
-    case 'percent':
-      return {
-        value: value,
-        prefix: '',
-        suffix: '%',
-      }
-    case 'euro':
-      return {
-        value: numberToString(currency(value,'EUR')),
-        prefix: '',
-        suffix: '',
-      }
-    case 'xcb':
-      return {
-        value: numberToString(currency(value)),
-        prefix: '',
-        suffix: '',
-      }
-    case 'time':
-      const d = new Date(value)
-      return {
-        value: numberToString(d.toLocaleString()),
-        prefix: '',
-        suffix: '',
-      }
-    case 'ago':
-      return {
-        value: numberToString(ago(value, true)),
-        prefix: '',
-        suffix: '',
-      }
-    case 'number':
-      return {
-        value: numberToString(numberFormat(value)),
-        prefix: '',
-        suffix: '',
-      }
-    default:
-      return {
-        value: numberToString(value),
-        prefix: '',
-        suffix: '',
-      }
-  }
+const getHashText = (value: string | number): TextType => ({
+  value: numberToString(siFormat(value, 2)),
+  prefix: '',
+  suffix: 'h/s',
+})
+
+const getPercentText = (value: string | number): TextType => ({
+  value: numberToString(value),
+  prefix: '',
+  suffix: '%',
+})
+
+const getEuroText = (value: string | number): TextType => ({
+  value: numberToString(currency(value, 'EUR')),
+  prefix: '',
+  suffix: '',
+})
+
+const getXCBText = (value: string | number): TextType => ({
+  value: numberToString(currency(value)),
+  prefix: '',
+  suffix: '',
+})
+
+const getTimeText = (value: string | number): TextType => ({
+  value: numberToString(new Date(value).toLocaleString()),
+  prefix: '',
+  suffix: '',
+})
+
+const getAgoText = (value: string): TextType => ({
+  value: numberToString(ago(value, true)),
+  prefix: '',
+  suffix: '',
+})
+
+const getNumberText = (value: string | number): TextType => ({
+  value: numberToString(numberFormat(value)),
+  prefix: '',
+  suffix: '',
+})
+
+const getDefaultText = (value: string | number): TextType => ({
+  value: numberToString(value),
+  prefix: '',
+  suffix: '',
+})
+
+const textTypeMapper = {
+  hashSpeed: getHashText,
+  hash: getHashText,
+  percent: getPercentText,
+  euro: getEuroText,
+  xcb: getXCBText,
+  time: getTimeText,
+  ago: getAgoText,
+  number: getNumberText,
+  default: getDefaultText,
+}
+
+export default function getText(type: TypeNumber, value): TextType {
+  return textTypeMapper[type ?? 'default'](value)
 }
