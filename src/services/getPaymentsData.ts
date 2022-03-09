c38d6ab75080170eb431c3975236d6eb4efa24c2
araad-wallit <araad.rashedi@wallit.eu>
2022-03-09 17:18:17 +0330

    feat: utilizing mock api for payments page

---
+++ src/services/getPaymentsData.ts
@@ -0,0 +1,67 @@
+import { toStringDateTime } from 'helpers/toStringDateTime'
+import { toXCBPrice } from 'helpers/toXCBPrice'
+
+import type { Column } from '@components/Table/Table'
+import type { InfoBoxItem } from 'helpers/text'
+
+const hydratePaymentsInfoBox = (data): InfoBoxItem[] => {
+  return [
+    { title: 'Sent payments', value: data.paymentsTotal, type: 'number' },
+    { title: 'XCB Price', value: 200, type: 'euro' }, // TODO Araad
+  ]
+}
+
+const hydratePaymentTableData = (payments) => {
+  const paymentMapper = (payment) => ({
+    'tx id': payment.tx,
+    address: payment.address,
+    amount: toXCBPrice(payment.amount),
+    time: toStringDateTime(payment.timestamp),
+  })
+
+  return payments.map(paymentMapper)
+}
+
+const hydratePaymentTableColumns = (): Column[] => {
+  return [
+    {
+      name: 'Time',
+      id: 'time',
+      type: 'time',
+    },
+    {
+      name: 'Amount',
+      id: 'amount',
+      type: 'xcb',
+    },
+    {
+      name: 'Address',
+      id: 'address',
+      type: 'address',
+    },
+    {
+      name: 'Tx id',
+      id: 'tx id',
+      type: 'tx',
+    },
+  ]
+}
+
+/**
+ * It fetches the data from the API, and then hydrates the data into the correct format
+ * @returns an object with two keys:
+ * - paymentsInfoBox: a hydrated version for the payments InfoBox component
+ * - payments: a hydrated version for the payment Table component
+ */
+export const getPaymentsData = async () => {
+  const result = await fetch(process.env.API_ENDPOINT + 'payments.json')
+  const data = await result.json()
+
+  return {
+    paymentsInfoBox: hydratePaymentsInfoBox(data),
+    paymentTable: {
+      data: hydratePaymentTableData(data.payments),
+      columns: hydratePaymentTableColumns(),
+    },
+  }
+}
