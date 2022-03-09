64e5c7a76deae977f6ca7590cb996b3392f3f909
araad-wallit <araad.rashedi@wallit.eu>
2022-03-09 17:07:56 +0330

    feat: utilizing mock api for home page jumbotron

---
+++ src/services/getHomeJumbotronData.ts
@@ -0,0 +1,32 @@
+const hydrateJumbotronData = (data) => {
+  const [node] = data.nodes
+  const { roundShares } = data.stats
+
+  return [
+    { title: 'Pool hashrate', value: data.hashrate, type: 'hashSpeed' },
+    { title: 'Network hashrate', value: 192900000, type: 'hashSpeed' }, // TODO Araad
+    { title: 'Network difficulty', value: node.difficulty, type: 'hash' },
+    { title: 'Active miners', value: data.minersTotal, type: 'number' },
+    {
+      title: 'Round variance',
+      value: (roundShares / node.difficulty).toFixed(2),
+      type: 'percent',
+    },
+  ]
+}
+
+/**
+ * It fetches the data from the API, and then hydrates the data into the correct format for the Jumbotron component
+ * @returns it resolves an array of objects for each of following properties:
+ *   - Pool hashrate
+ *   - Network hashrate
+ *   - Network difficulty
+ *   - Active miners
+ *   - Round variance
+ */
+export const getHomeJumbotronData = async () => {
+  const result = await fetch(process.env.API_ENDPOINT + 'stats.json')
+  const data = await result.json()
+
+  return hydrateJumbotronData(data)
+}
