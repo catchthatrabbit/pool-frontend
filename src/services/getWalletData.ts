2a81ad27a57a2023501c254266e18cc89e8f0a2c
araad-wallit <araad.rashedi@wallit.eu>
2022-03-31 23:49:36 +0430

    feat: [#20] aggregation on wallet page data

--- src/services/getWalletData.ts
+++ src/services/getWalletData.ts
@@ -1,19 +1,30 @@
+import {
+  aggregateNumbers,
+  fetchAllSettled,
+  mergeArraysAndObjects,
+  reduceList,
+} from 'helpers'
 import { toStringDateTime } from 'helpers/toStringDateTime'
 import { toXCBPrice } from 'helpers/toXCBPrice'
 
 import type { Column } from '@components/Table/Table'
+import { AGGREGATE_API_ENDPOINTS } from 'config/api-endpoints.config'
 
+const hydrateWorkersTableData = (workers: any) => {
+  if (!workers) return []
 
-const hydrateWorkersTableData = (workers: any[]) => {
   const workerMapper = ([id, detail]) => ({
     id,
     hashShort: detail.hr,
     hashLong: detail.hr2,
     last: toStringDateTime(detail.lastBeat),
+    lastBeat: detail.lastBeat,
     status: detail.offline,
   })
 
-  return Object.entries(workers).map(workerMapper)
+  return Object.entries(workers)
+    .map(workerMapper)
+    .sort((a, b) => (a.lastBeat < b.lastBeat ? 1 : -1))
 }
 
 const hydrateWorkersTableColumns = (): Column[] => {
@@ -46,14 +57,18 @@ const hydrateWorkersTableColumns = (): Column[] => {
   ]
 }
 
-const hydratePayoutsTableData = (payouts) => {
+const hydratePayoutsTableData = (payouts: any[]) => {
+  if (!payouts) return []
+
   const payoutMapper = (payout) => ({
     time: toStringDateTime(payout.timestamp),
     tx: payout.tx,
     amount: toXCBPrice(payout.amount),
   })
 
-  return payouts.map(payoutMapper)
+  return payouts
+    .sort((a, b) => (a.timestamp < b.timestamp ? 1 : -1))
+    .map(payoutMapper)
 }
 
 const hydratePayoutsTableColumns = (): Column[] => {
@@ -77,6 +92,8 @@ const hydratePayoutsTableColumns = (): Column[] => {
 }
 
 const hydrateMinerInfoData = (data) => {
+  if (!data) return []
+
   const computingInformation = {
     title: 'Computing information',
     data: [
@@ -86,16 +103,36 @@ const hydrateMinerInfoData = (data) => {
         value: data.workersOnline,
         type: 'number',
       },
-      { key: '2', title: 'Hashrate ~30m', value: data.currentHashrate, type: 'hashrate' },
-      { key: '3', title: 'Hashrate ~3h', value: data.hashrate, type: 'hashrate' },
-      { key: '4', title: 'Last share', value: toStringDateTime(data.stats?.lastShare), type: 'ago' },
+      {
+        key: '2',
+        title: 'Hashrate ~30m',
+        value: data.currentHashrate,
+        type: 'hashrate',
+      },
+      {
+        key: '3',
+        title: 'Hashrate ~3h',
+        value: data.hashrate,
+        type: 'hashrate',
+      },
+      {
+        key: '4',
+        title: 'Last share',
+        value: toStringDateTime(data.lastShare),
+        type: 'ago',
+      },
       {
         key: '5',
         title: 'Round share',
         value: data.roundShares,
         type: 'percentage',
       },
-      { key: '6', title: 'Blocks found', value: data.stats?.blocksFound ?? 0, type: 'number' },
+      {
+        key: '6',
+        title: 'Blocks found',
+        value: data.blocksFound ?? 0,
+        type: 'number',
+      },
     ],
   }
 
@@ -105,13 +142,13 @@ const hydrateMinerInfoData = (data) => {
       {
         key: '1',
         title: 'Immature balance',
-        value: toXCBPrice(data.stats.immature),
+        value: toXCBPrice(data.immature),
         type: 'xcb',
       },
       {
         key: '2',
         title: 'Pending balance',
-        value: toXCBPrice(data.stats.pending),
+        value: toXCBPrice(data.pending),
         type: 'xcb',
       },
       {
@@ -123,7 +160,7 @@ const hydrateMinerInfoData = (data) => {
       {
         key: '4',
         title: 'Total paid',
-        value: toXCBPrice(data.stats.paid),
+        value: toXCBPrice(data.paid),
         type: 'xcb',
       },
     ],
@@ -138,21 +175,57 @@ const hydrateMinerInfoData = (data) => {
  * - workersInfoBox: a hydrated version for the workers InfoBox component
  * - workerTable: a hydrated version for the worker Table component
  */
-export const getWalletData = async (address: string) => {
-  // TODO Araad: use "address"
-  const result = await fetch(process.env.API_ENDPOINT + 'accounts-ab623f7c365a5da4b3b00bf0a54a1887ac528eeaaa86.json')
-  const status = result.ok ? false : result.status
-  const data = await result.json()
+export const getWalletData = async (address: string): Promise<any> => {
+  const allAccounts = await fetchAllSettled(
+    AGGREGATE_API_ENDPOINTS.map((endpoint) => `${endpoint}accounts/${address}`),
+  )
+
+  //
+  // separating stats and tables data from each other
+  const { allAccountsStatsData, allAccountsTableData } = allAccounts.reduce(
+    (acc, item) => {
+      const { stats, workers, payments, ...rest } = item
+
+      acc.allAccountsStatsData = [
+        ...acc.allAccountsStatsData,
+        { ...stats, ...rest },
+      ]
+      acc.allAccountsTableData = [
+        ...acc.allAccountsTableData,
+        { workers, payments },
+      ]
+
+      return acc
+    },
+    { allAccountsStatsData: [], allAccountsTableData: [] },
+  )
+
+  const aggregator = aggregateNumbers([
+    'currentHashrate',
+    'hashrate',
+    'balance',
+    'blocksFound',
+    'immature',
+    'paid',
+    'pending',
+    'roundShares',
+    'paymentsTotal',
+    'workersOffline',
+    'workersOnline',
+    'workersTotal',
+  ])
+  const stats = reduceList<any>(allAccountsStatsData, aggregator)
+  const account = reduceList<any>(allAccountsTableData, mergeArraysAndObjects)
 
   return {
-    status,
-    minerInfo: hydrateMinerInfoData(data),
+    isNotFound: !account,
+    minerInfo: hydrateMinerInfoData(stats),
     workersTable: {
-      data: hydrateWorkersTableData(data.workers),
+      data: hydrateWorkersTableData(account?.workers),
       columns: hydrateWorkersTableColumns(),
     },
     payoutsTable: {
-      data: hydratePayoutsTableData(data.payments),
+      data: hydratePayoutsTableData(account?.payments),
       columns: hydratePayoutsTableColumns(),
     },
   }
